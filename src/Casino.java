dda2d6b84201e2f4928e602a94317ef9a72b1fe0
HansiDenis <denis@91240@gmail.com>
2019-12-09 09:35:05 +0100

    Ajout classe Casino

--- src/Casino.java
+++ src/Casino.java
@@ -1,10 +1,10 @@
 import java.util.HashMap;
 
 public class Casino {
-    private static HashMap<Integer, String[]> Roulette;
+    public static HashMap<Integer, String[]> Roulette = new HashMap<Integer, String[]>(37);
 
     public void create() {
-        Roulette.put(0, new String[]{"pair", "vert"});
+        Roulette.put(0, new String[]{"zéro", "vert"});
         for (int i = 1; i <= 36; i++) {
             if (i <= 10) {
                 if (i % 2 == 0) {
@@ -16,7 +16,7 @@ public class Casino {
                 if (i % 2 == 0) {
                     Roulette.put(i, new String[]{"pair", "rouge"});
                 } else {
-                    Roulette.put(i, new String[]{"imppair", "noir"});
+                    Roulette.put(i, new String[]{"impair", "noir"});
                 }
             } else if (i > 18 && i <= 28) {
                 if (i % 2 == 0) {
@@ -27,8 +27,11 @@ public class Casino {
             } else {
                 if (i % 2 == 0) {
                     Roulette.put(i, new String[]{"pair", "rouge"});
-                } else Roulette.put(i, new String[]{"imppair", "noir"});
+                } else Roulette.put(i, new String[]{"impair", "noir"});
             }
         }
+        /**for (int i=0;i<=36;i++){
+         System.out.println(Arrays.toString(Roulette.get(i)));
+         }**/
     }
 }
