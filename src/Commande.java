1a2389cfc0cb965afd1fbd481cad096291eeff90
Salome-m <yungkyrie91@gmail.com>
2019-12-09 11:15:07 +0100

    Ajout classes restaurant et commande

---
+++ src/Commande.java
@@ -0,0 +1,73 @@
+public class Commande {
+
+    boolean[] commande = new boolean[5];
+    private double prix;
+
+    public Commande() {
+        this.prix = 0.0;
+    }
+
+    public void commandeFormule(String rep) {
+        if (rep.equals("Mojito")) {
+            prix = 6.0;
+            commande[1] = true;
+            commande[4] = true;
+        } else if (rep.equals("Daiquiri")) {
+            prix = 6.0;
+            commande[1] = true;
+            commande[2] = true;
+            commande[4] = true;
+        }
+
+    }
+
+    public double commandeCarte(boolean entrée, boolean plat, boolean accompagnement, boolean dessert, boolean boisson) {
+        if (entrée) {
+            commande[0] = entrée;
+            prix += 3.0;
+        } else if (plat) {
+            commande[1] = plat;
+            prix += 5.0;
+        } else if (accompagnement) {
+            commande[2] = accompagnement;
+            prix += 2.0;
+        } else if (dessert) {
+            commande[3] = dessert;
+            prix += 3.5;
+        } else if (boisson) {
+            commande[4] = boisson;
+            prix += 2.0;
+        }
+        return prix;
+    }
+
+    public double addMojito(boolean entrée, boolean accompagnement, boolean dessert) {
+        if (entrée) {
+            commande[0] = entrée;
+            prix += 3.0;
+        } else if (accompagnement) {
+            commande[2] = accompagnement;
+            prix += 2.0;
+        } else if (dessert) {
+            commande[3] = dessert;
+            prix += 3.5;
+        }
+        return prix;
+    }
+
+    public double addDaiquiri(boolean entrée, boolean dessert) {
+        if (entrée) {
+            commande[0] = entrée;
+            prix += 3.0;
+        } else if (dessert) {
+            commande[3] = dessert;
+            prix += 3.5;
+        }
+        return prix;
+    }
+
+    public void useTickets(int nb) {
+        prix = prix - nb * 5;
+    }
+
+}
---
+++ src/Restaurant.java
@@ -0,0 +1,13 @@
+public class Restaurant {
+
+    private static String carte = "Plat : 5€\nAccompagnement : 2€\nEntrée : 3€\nDessert : 3.50€\nBoisson : 2€\n\nFormules :\n\"Mojito\" : plat + boisson pour 6€\n" +
+            "\"Daiquiri\"" + " : plat + accompagnement + boisson pour 8€";
+
+    public Restaurant() {
+
+    }
+
+    public void afficheCarte() {
+        System.out.println(this.carte);
+    }
+}
