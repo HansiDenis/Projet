9eefd8c0b6184550ad2bb09c230b18c54b1b2a18
Salome-m <yungkyrie91@gmail.com>
2020-04-14 15:45:07 +0200

    Ajout classe FoodAndDrink

--- src/Projet/Comedy.java
+++ src/Projet/Comedy.java
@@ -1,4 +1,6 @@
 package Projet;
 
 public class Comedy {
+
+
 }
--- src/Projet/FoodAndDrink.java
+++ src/Projet/FoodAndDrink.java
@@ -6,18 +6,22 @@ public class FoodAndDrink extends Event {
 
     public String kind; //le type, ie si c'est un restaurant, un bar, etc
 
-    FoodAndDrink(int[] date, String place, String name, String genre) {
-        this.date = date;
+    FoodAndDrink(int start, int end, String place, String name, String genre) {
+        date = new int[2];
+        this.date[0] = start;
+        this.date[1] = end;
         this.place = place;
         this.name = name;
         this.type = "Food & Drink";
         this.kind = genre;
     }
 
+    @Override
     void takePlace(List<Event> newEvents) {
         System.out.println("");
     }
 
+    @Override
     String reference() {
         if (kind.length() <= 2) {
             return "efd" + this.kind;
