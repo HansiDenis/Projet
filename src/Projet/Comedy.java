2b5070510ed09da36e094900a3b92a623bdae91f
Salome-m <yungkyrie91@gmail.com>
2020-04-14 15:57:45 +0200

    Ajout classe FoodAndDrink

--- src/Projet/Comedy.java
+++ src/Projet/Comedy.java
@@ -1,6 +1,41 @@
 package Projet;
 
-public class Comedy {
+import java.util.List;
+
+public class Comedy extends Event {
+
+    String comedy;
+    int duree;
+    int note;
+
+    Comedy(int start, int end, String place, String name, String comedy, int note) {
+        date = new date[2];
+        this.date[0] = start;
+        this.date[1] = end;
+        this.place = place;
+        this.name = name;
+        this.type = "Comedy";
+        this.comedy = comedy;
+        if (comedy.equals("one-man show")) {
+            this.note = note;
+        }
+        this.duree = duree;
+    }
+
+    @Override
+    void takePlace(List<Event> newEvents) {
+
+        System.out.println("La comédie est là pour vous faire rire et vous changez les idées.");
+        if (comedy.equals("one-man show")) {
+            System.out.println("si vous le souhaiter, vous pouvez mettre une note sur tripadvisor.");
+        }
+    }
+
+    @Override
+    String reference() {
+        return "SC" + comedy.substring(0, Integer.min(3, comedy.length()));
+    }
+}
 
 
 }
