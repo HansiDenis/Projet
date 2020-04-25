d7ca15b1261067a5b59deca29fa5975c211d4430
Salome-m <yungkyrie91@gmail.com>
2020-04-25 12:06:21 +0200

    l

---
+++ src/Tests/RefSearchTest.java
@@ -0,0 +1,45 @@
+package Tests;
+
+import events.*;
+
+public class RefSearchTest {
+
+    Event e1, e2;
+    String ref1, ref2;
+
+    public RefSearchTest() {
+        e1 = new NBASeries("WorldCup", "China", 100, 120, "Lakers", "Clippers", 20000);
+        e2 = new FoodAndDrink(5,42,"antibes","bk","fastfood","burger");
+        ref1 = e1.reference();
+        ref2 = e2.reference();
+    }
+
+
+    public void positiveTest() {
+        Event res = AllEvents.getInstance().refSearch("FDE-fas-2-5-42");
+        System.out.println("Test d'une reference existante : ");
+        System.out.println("Evenement trouvé : " + res + "\nRéférence correspondante : " + res.reference());
+    }
+
+    public void negativeTest(){
+        System.out.println("Test d'une référence non existante : ");
+        Event res = AllEvents.getInstance().refSearch("FDE");
+
+    }
+
+    public void refSearchTest(){
+        System.out.println("Tests positifs : ");
+        positiveTest();
+        System.out.println("Tests négatifs : ");
+        negativeTest();
+    }
+
+    public static void main(String[] args) {
+
+        RefSearchTest rs = new RefSearchTest();
+        rs.refSearchTest();
+    }
+
+}
+
+
